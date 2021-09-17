18c0a2268761c206f6e366052b11e5a33db540a2
blvckcenturion <mr.plus.co@gmail.com>
2021-09-17 10:00:34 -0400

    Initial commit

---
+++ dev12222/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebAPIClient;
+
+namespace dev12222
+{
+    class Program
+    {
+        
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new HttpClient();
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"),
+            };
+
+            var streamTask = client.GetStreamAsync("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+            var criptos = await JsonSerializer.DeserializeAsync<List<CriptoAsset>>(await streamTask);
+
+            Console.WriteLine(criptos);
+            foreach (var cripto in criptos)
+            {
+                Console.WriteLine(cripto.symbol);
+                Console.WriteLine(cripto.name);
+                Console.WriteLine(cripto.current_price);
+                Console.WriteLine(cripto.market_cap);
+                Console.WriteLine(cripto.market_cap_rank);
+                
+            }
+            
+
+        }
+    }
+}
---
+++ dev12222/repo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebAPIClient
+{
+    public class CriptoAsset
+    {
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public double current_price { get; set; }
+        public double market_cap_rank { get; set; }
+        public double market_cap { get; set; }
+        public double image { get; set; }
+        public double price_change_percentage_24h { get; set; }
+        public double high_24h { get; set; }
+        public double low_24h { get; set; }
+        public double circulating_supply { get; set; }
+        public double ath { get; set; }   
+    }
+}
